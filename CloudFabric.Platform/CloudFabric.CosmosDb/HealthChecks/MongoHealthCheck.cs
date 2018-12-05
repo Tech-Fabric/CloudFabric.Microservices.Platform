155ae39bd90b91170f428854c00771a3b69ae8be
Preetham Reddy <preetham@techfabric.co>
2018-12-04 19:58:06 -0700

    Adding CosmosDB HealthChecks

---
+++ CloudFabric.Platform/CloudFabric.CosmosDb/HealthChecks/MongoHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CloudFabric.CosmosDb.HealthChecks
+{
+    public class MongoHealthCheck : IHealthCheck
+    {
+        string _connectionString;
+
+        public string Name => "mongo";
+
+        public MongoHealthCheck(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var _client = new MongoClient(_connectionString);
+                await _client.ListDatabasesAsync();
+            }
+            catch (Exception)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
---
+++ CloudFabric.Platform/CloudFabric.CosmosDb/HealthChecks/SQLHealthCheck.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudFabric.CosmosDb.HealthChecks
+{
+    public class SQLHealthCheck : IHealthCheck
+    {
+        string _uri, _sharedAccessKey;
+
+        public string Name => "sql";
+
+        public SQLHealthCheck(string uri, string sharedAccessKey)
+        {
+            _uri = uri;
+            _sharedAccessKey = sharedAccessKey;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var _client = new DocumentClient(new Uri(_uri), _sharedAccessKey); ;
+                var _account = await _client.GetDatabaseAccountAsync();
+
+                if(_account == null)
+                {
+                    return HealthCheckResult.Unhealthy();
+                }
+            }
+            catch (Exception)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
